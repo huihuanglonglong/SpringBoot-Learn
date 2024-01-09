30183f4fa4e3e04a569ea0f7544707294f494257
huihuanglonglong <2050563823@qq.com>
2024-01-10 00:12:37 +0800

    提交算法练习2

--- src/main/java/org/lyl/common/util/PageUtil.java
+++ src/main/java/org/lyl/common/util/PageUtil.java
@@ -1,6 +1,12 @@
 package org.lyl.common.util;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PageUtil {
 
@@ -12,8 +18,8 @@ public class PageUtil {
      * @param <T>
      * @return
      */
-    public static <T> List<List<T>> partitionByPage(List<T> dataList) {
-        return null;
+    public static <T> List<List<T>> partitionByPage(List<T> dataList, int batch) {
+        return Lists.partition(dataList, batch);
     }
 
     /**
@@ -23,10 +29,21 @@ public class PageUtil {
      * @param <T>
      * @return
      */
-    public static <T> List<List<T>> partitionByStream(List<T> dataList) {
-
-        return null;
+    public static <T> List<List<T>> partitionByStream(List<T> dataList, int batch) {
+        List<List<T>> partitionList = Lists.newArrayList();
+        int maxPageNum = (dataList.size() % batch == 0) ? (dataList.size() / batch) : ((dataList.size() / batch) + 1);
+        for (int i = 0; i < maxPageNum; i++) {
+            partitionList.add(dataList.stream().skip(i * batch).limit(batch).collect(Collectors.toList()));
+        }
+        return partitionList;
     }
+
+
+    /*public static void main(String[] args) {
+        List<Integer> dataList = Lists.newArrayList(1, 3, 5, 7, 9, 11, 13, 15, 17, 20);
+        List<List<Integer>> partitionList = partitionByStream(dataList, 3);
+        partitionList.forEach(System.out::println);
+    }*/
 
 
 }
