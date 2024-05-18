baa30508face4edd0e1f443564c746c0c704dee5
huihuanglonglong <2050563823@qq.com>
2024-05-18 17:56:51 +0800

    添加Spring容器功能

--- src/main/java/org/lyl/common/util/ApplicationContextUtil.java
+++ src/main/java/org/lyl/common/util/ApplicationContextUtil.java
@@ -3,14 +3,17 @@ package org.lyl.common.util;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.aop.framework.AdvisedSupport;
 import org.springframework.aop.framework.AopProxy;
+import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.ResolvableType;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Objects;
 
 
 /**
@@ -59,10 +62,43 @@ public class ApplicationContextUtil implements ApplicationContextAware {
         return ObjectUtils.isEmpty(targetType)? defaultVal : property;
     }
 
+    /**
+     * 从Spring容器 通过父类泛型获取子类Bean
+     *  Aclass extends AbsClass<XXXX>
+     *
+     * @param sourceClass
+     * @param superGenericClass
+     * @return
+     */
+    public static Object getBeanBySuperGenericType(Class<?> sourceClass, Class<?> superGenericClass) {
+        final Object[] targetBeans = new Object[1];
+        Map<String, ?> beansMap = getBeansByType(sourceClass);
+        beansMap.forEach((beanName, bean) -> {
+            Class<?> rawClass = ResolvableType.forClass(bean.getClass()).getSuperType().getGeneric(0).getRawClass();
+            if (Objects.nonNull(rawClass) && superGenericClass.getName().equals(rawClass.getName())) {
+                targetBeans[0] = bean;
+            }
+        });
+        return targetBeans[0];
+    }
+
 
     /******************************* 代理相关 *******************************************/
+
     /**
-     * 获取 目标对象
+     * 通过代理对象获取 目标对象---方案一
+     *
+     * @param proxy
+     * @param <T>
+     * @return
+     */
+    public static <T> T getAopProxySourceBean(T proxy) {
+        return (T) AopProxyUtils.getSingletonTarget(proxy);
+    }
+
+
+    /**
+     * 通过代理对象获取 目标对象---方案二
      * @param proxy 代理对象
      * @return
      * @throws Exception
@@ -84,6 +120,7 @@ public class ApplicationContextUtil implements ApplicationContextAware {
         Field advisedField = dynamicAdvisedInterceptor.getClass().getDeclaredField("advised");
         advisedField.setAccessible(true);
         Object targetObj = ((AdvisedSupport)advisedField.get(dynamicAdvisedInterceptor)).getTargetSource().getTarget();
+        advisedField.setAccessible(false);
         return targetObj;
     }
 
@@ -97,6 +134,7 @@ public class ApplicationContextUtil implements ApplicationContextAware {
         advisedField.setAccessible(true);
 
         Object target = ((AdvisedSupport)advisedField.get(aopProxy)).getTargetSource().getTarget();
+        advisedField.setAccessible(false);
         return target;
     }
 
